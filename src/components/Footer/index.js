9a7a1c0526c95c54c4ddf40a1103a4079b57d670
Alex Eftimiades <alexeftimiades@gmail.com>
2018-01-15 20:36:46 -0500

    Change logo
    - It's not powered by coinbase, it's powered by 'Merica!

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,14 +7,14 @@ const Footer = () => (
   <div className="Footer">
     <span>Powered by </span>
     <a
-      href="https://developers.coinbase.com/api/v2"
+      href="https://www.usa.gov"
       target="_blank"
       rel="noopener noreferrer"
     >
       <img
         src={logo}
         className="logo"
-        alt="coinbase"
+        alt="Fuck yeah"
       />
     </a>
   </div>
