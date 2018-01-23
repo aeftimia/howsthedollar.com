01b0082317e186909fe5e0392750bc7896945f49
Alex Eftimiades <alexeftimiades@gmail.com>
2018-01-22 21:42:00 -0500

    Add share icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { ShareButtons, generateShareIcon } from 'react-share';
 
 import logo from './assets/logo.svg';
 import './index.css';
+
+const {
+  FacebookShareButton,
+  GooglePlusShareButton,
+  TwitterShareButton,
+  RedditShareButton,
+} = ShareButtons;
+
+const FacebookIcon = generateShareIcon('facebook');
+const TwitterIcon = generateShareIcon('twitter');
+const GooglePlusIcon = generateShareIcon('google');
+const RedditIcon = generateShareIcon('reddit');
+
+const shareUrl = 'https://howsthedollar.com';
+const title = 'How\'s The Dollar?';
 
 const Footer = () => (
   <div className="Footer">
@@ -17,7 +33,37 @@ const Footer = () => (
         alt="Fuck yeah"
       />
     </a>
+    <div />
+    <TwitterShareButton
+      url={shareUrl}
+      title={title}
+      className="share share-button"
+    >
+      <TwitterIcon size={32} round />
+    </TwitterShareButton>
+    <FacebookShareButton
+      url={shareUrl}
+      title={title}
+      className="share share-button"
+    >
+      <FacebookIcon size={32} round />
+    </FacebookShareButton>
+    <RedditShareButton
+      url={shareUrl}
+      title={title}
+      className="share share-button"
+    >
+      <RedditIcon size={32} round />
+    </RedditShareButton>
+    <GooglePlusShareButton
+      url={shareUrl}
+      title={title}
+      className="share share-button"
+    >
+      <GooglePlusIcon size={32} round />
+    </GooglePlusShareButton>
   </div>
 );
 
 export default Footer;
+
